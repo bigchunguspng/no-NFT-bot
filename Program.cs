156cd349854e105c197c7082ed10499515c9892c
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2024-07-26 14:05:43 +0300

    # 3

--- Program.cs
+++ Program.cs
@@ -15,20 +15,42 @@ internal static class Program
         {
             var bot = new TelegramBotClient(File.ReadAllText("token").Trim());
 
+            LogEnter(bot);
+
             var options = new ReceiverOptions
             {
                 AllowedUpdates = [UpdateType.Message, UpdateType.EditedMessage]
             };
 
             bot.StartReceiving(HandleUpdate, HandlePollingError, options);
+
+            WaitForExit();
         }
         catch (Exception e)
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine($"MAIN LOOP >> MEGA BRUH -> {e.Message}");
-            Console.ResetColor();
             Console.ReadKey();
         }
+    }
+
+    private static void LogEnter(TelegramBotClient bot)
+    {
+        var me = bot.GetMeAsync().Result;
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"MAIN LOOP >> ENTERING AS [{me.FirstName}] / @{me.Username}");
+    }
+
+    private static void WaitForExit()
+    {
+        ConsoleKeyInfo input = default;
+        while (input.Key != ConsoleKey.Q)
+        {
+            Console.ResetColor();
+            input = Console.ReadKey();
+        }
+        
     }
 
     private static Task HandleUpdate(ITelegramBotClient bot, Update update, CancellationToken token)
@@ -53,7 +75,6 @@ internal static class Program
 
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine($"{chat} / {title} >> BRUH -> {exception.Message}");
-            Console.ResetColor();
         }
     }
 
@@ -69,14 +90,17 @@ internal static class Program
             var chat = message.Chat.Id;
             var title = message.Chat.Title ?? string.Empty;
 
-            Console.WriteLine($"{chat} / {title} >> NFT SPAM DETECTED!");
+            Console.ResetColor();
+            Console.WriteLine($"{chat} / {title} >> NFT SPAM DETECTED");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"{chat} / {title} >> {text}");
 
             var member = await bot.GetChatMemberAsync(message.Chat.Id, message.From.Id);
             if (member.Status is not ChatMemberStatus.Administrator and not ChatMemberStatus.Member)
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine($"{chat} / {title} >> DELETING NFT SPAM!");
-                Console.ResetColor();
+                Console.WriteLine($"{chat} / {title} >> DELETING NFT SPAM!"); ;
 
                 await bot.DeleteMessageAsync(message.Chat.Id, message.MessageId);
             }
@@ -87,7 +111,6 @@ internal static class Program
     {
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine($"Telegram API Error x_x --> {exception.Message}");
-        Console.ResetColor();
 
         return Task.CompletedTask;
     }
